d64246b91f2b7fe80161f114c5002dce3217d04b
Kat Daly <katiya.daly@gmail.com>
2016-10-28 18:19:58 -0400

    continued trying to write this python script

--- wk8_genome3d/breakheat.py
+++ wk8_genome3d/breakheat.py
@@ -13,11 +13,39 @@ file.keys()
 counts = file['0.counts'][...]
 e = file['0.expected'][...]
 positions = file['0.positions'][...]
+regions = file['regions'][...]
+
 
 ennnnnrichments = (counts/e)
 ennnnnrichments = np.log10(ennnnnrichments +1)
 
+dict_ctcf = {}
 
-print control
+
+# print regions
+# quit()
+
+
+# need to run through ctcf file and compare positions to 5C
+for i, line in enumerate(control):
+    ctcf = line.rstrip( "\r\n" ).split('\t')
+    if line.startswith('#Chromosome'):
+        continue
+    if ctcf[0] == 'chrX':
+         dict_ctcf[ctcf[1]] = ctcf[1]
+        # print ctcf_pos
+        # quit()
+
+#now I want to compare to positions of the enrichments. 
+
+
+for p, startstop in enumerate(positions):
+    for element in dict_ctcf:
+        # print startstop
+        if element > startstop[0]:
+            if element < startstop[1]:
+                print positions
+            
+
 
 
